f9b775217e058e0b0c3a2f9662ae65c910fd5518
agapesinuhaji <agapemanase27@gmail.com>
2024-04-22 15:36:18 +0700

    Stateless & Stateful Widget

---
+++ belajar_stateful_widget/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int number = 0;
+  void tekanTombol() {
+    number = number + 1;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Stateful Widget Demo"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                number.toString(),
+                style: TextStyle(fontSize: 10 + number.toDouble()),
+              ),
+              ElevatedButton(
+                child: Text("Tambah Bilangan"),
+                onPressed: tekanTombol,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
